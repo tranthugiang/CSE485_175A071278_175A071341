5bbca83095291b155329234217d26dc4b1665679
tranthugiang <giangtt72@wru.vn>
2019-10-10 11:06:17 +0700

    giang vua tai len tep moi

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(document).scrollTop() >= 200 && $('#header_menu.header-menu').hasClass('fixed') == false) {
+            $('#header_menu.header-menu').addClass('fixed');
+        }
+        if($(document).scrollTop() < 200 && $('#header_menu.header-menu').hasClass('fixed') == true) {
+            $('#header_menu.header-menu').removeClass('fixed');
+        }
+        if($(document).scrollTop() >=1232){
+           $('.slide-cauhoi1').slideDown(1300);
+        }
+        if($(document).scrollTop() <=1200){
+            $('.slide-cauhoi1').hide();
+         }
+         if($(document).scrollTop() >=1600){
+            $('.slide-cauhoi2').slideDown(1300);
+         }
+         if($(document).scrollTop() <=1474){
+             $('.slide-cauhoi2').hide();
+          }
+          if($(document).scrollTop() >=2000){
+            $('.container-img-yeucau').fadeIn(1300);
+         }
+         if($(document).scrollTop() <=1900){
+             $('.container-img-yeucau').fadeOut(800);
+          }
+        console.log('window position=', $(window).scrollTop());
+    });
+});
